290be0631eb79392646a1016d414929155da94e9
Zakhar-T <zakhar.null@gmail.com>
2025-05-07 22:04:12 +0300

    edit adtg, ftrs and global fixes

--- src/js/advantagesSwiper.js
+++ src/js/advantagesSwiper.js
@@ -2,23 +2,28 @@ import Swiper from 'swiper';
 import { Keyboard, Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
 import 'swiper/css/navigation';
+import 'swiper/scss/keyboard';
 
 new Swiper('.advtg-swiper-js', {
+  modules: [Keyboard, Navigation],
+  height: 280,
   loop: true,
   slidesPerView: 'auto',
   centeredSlides: true,
-  grid: {
-    rows: 2,
-  },
   spaceBetween: 8,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
   breakpoints: {
     1200: {
+      height: 500,
       slidesPerView: 2,
       spaceBetween: 24,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+      navigation: {
+        prevEl: '[data-advtg-swiper-prev]',
+        nextEl: '[data-advtg-swiper-next]',
+      },
     },
   },
 });
--- src/js/featuresSwiper.js
+++ src/js/featuresSwiper.js
@@ -1,19 +1,44 @@
 import Swiper from 'swiper';
 import { Keyboard, Navigation } from 'swiper/modules';
 import 'swiper/css/bundle';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
+import 'swiper/scss/keyboard';
+import 'swiper/bundle';
 
 new Swiper('.ftrs-swiper-js', {
+  modules: [Keyboard, Navigation],
   loop: true,
   slidesPerView: 'auto',
-  watchOverflow: false,
-  height: 203,
-  resistance: false,
+  watchOverflow: true,
   centeredSlides: true,
-  centeredSlidesBounds: true,
   spaceBetween: 12,
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
+  breakpoints: {
+    320: {
+      height: 203,
+      spaceBetween: 12,
+    },
+    1200: {
+      height: 386,
+      observer: true,
+      observeParents: true,
+      grabCursor: true,
+      effect: 'coverflow',
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      navigation: {
+        prevEl: '[data-ftrs-swiper-prev]',
+        nextEl: '[data-ftrs-swiper-next]',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+      },
+    },
   },
 });
